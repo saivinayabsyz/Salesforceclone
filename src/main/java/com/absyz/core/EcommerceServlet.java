4374b36845e74c94685973d28ee90f17619055c2
saivinayabsyz <30690019+saivinayabsyz@users.noreply.github.com>
2020-01-06 15:12:25 +0530

    Update EcommerceServlet.java

--- src/main/java/com/absyz/core/EcommerceServlet.java
+++ src/main/java/com/absyz/core/EcommerceServlet.java
@@ -32,16 +32,7 @@ public class EcommerceServlet extends HttpServlet {
    public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 	   try {
-		if (request.getParameter("serviceId").equals("cloneorg"))
-		   {
-			  
-			String strOutput = UserRegistration.user_reg(request);
-			
-response.setContentType("application/json");
-			      // Actual logic goes here.
-			      PrintWriter out = response.getWriter();
-			      out.println("<h1>" + strOutput + "</h1>");
-		   }
+		
 	} catch (JSONException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -52,118 +43,15 @@ response.setContentType("application/json");
    public void doPost(HttpServletRequest request, HttpServletResponse response)
 		      throws ServletException, IOException{
 	   try {
-		if (request.getParameter("serviceId").equals("userreg"))
+		
+	if (request.getParameter("serviceId").equals("cloneorg"))
 		   {
-			   String strOutput = UserRegistration.user_reg(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		  
-		if (request.getParameter("serviceId").equals("changepwd"))
-		   {
-			   String strOutput = UserRegistration.change_password(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("login"))
-		   {
-			   String strOutput = LoginService.userLogin(request);
-			   	      
-			      response.setContentType("text/html");
-			      response.setHeader("Cache-Control", "no-cache");
-			      response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("adminlogin"))
-		   {
-			   String strOutput = LoginService.adminLogin(request);
-			   	      
-			      response.setContentType("text/html");
-			      response.setHeader("Cache-Control", "no-cache");
-			      response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("showuser"))
-		   {
-			   String strOutput = LoginService.show_user(request);
-			   	      
-			      response.setContentType("text/html");
-			      response.setHeader("Cache-Control", "no-cache");
-			      response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("addproduct"))
-		   {
-			   String strOutput =Products.add_products(request);
-			   response.setContentType("text/html");
-				response.setHeader("Cache-Control", "no-cache");
-				response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("addtocart"))
-		   {
-			   String strOutput =Carts.add_to_cart(request);
-			   response.setContentType("text/html");
-				response.setHeader("Cache-Control", "no-cache");
-				response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("deletecart"))
-		   {
-			   System.out.println("Inside delete block");
-			   String strOutput =Carts.remove_cart(request);
-			   response.setContentType("text/html");
-				response.setHeader("Cache-Control", "no-cache");
-				response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("orders"))
-		   {
-			   String strOutput =Orders.new_order(request);
-			   response.setContentType("text/html");
-				response.setHeader("Cache-Control", "no-cache");
-				response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		     if (request.getParameter("serviceId").equals("changestatus"))
-		   {
-			   String strOutput =Orders.changestatus(request);
-			   response.setContentType("text/html");
-				response.setHeader("Cache-Control", "no-cache");
-				response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("shipping"))
-		   {
-			   String strOutput =UserRegistration.add_shipping_address(request);
-			      // Set response content type
-			      response.setContentType("text/html");
-
+			  
+			String strOutput = RetrieveSample.startretrieve(request);
+			response.setContentType("application/json");
 			      // Actual logic goes here.
 			      PrintWriter out = response.getWriter();
 			      out.println("<h1>" + strOutput + "</h1>");
-		   }
-		   if (request.getParameter("serviceId").equals("myorders"))
-		   {
-			   String strOutput =Orders.my_order_list(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("show_products"))
-		   {
-			   String strOutput = Products.show_products(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("show_productinfo"))
-		   {
-			   String strOutput = Products.show_productinfo(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
-		   }
-		   if (request.getParameter("serviceId").equals("mycarts"))
-		   {
-			   String strOutput =Carts.my_cart_list(request);
-			   response.setContentType("text/html");
-			   response.setHeader("Cache-Control", "no-cache");
-			   response.getOutputStream().write(new String(strOutput.getBytes("UTF-8")).getBytes());
 		   }
 		    
 	} catch (JSONException e) {
